d20b600a0fc0a3837ef2cb47e33d3114daecbd17
FriskGD <111191685+FriskGD@users.noreply.github.com>
2022-08-13 09:55:32 -0600

    Add files via upload

---
+++ Ejercicio 12/pitagoras.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+ab = float(input('Ingrese el valor de la las longitudes del vértice A y B: '))
+bc = float(input('Ingrese el valor de la las longitudes del vértice B y C: '))
+
+hipotenusa = sqrt(ab**2)
+
+print('La longitud de la hipotenusa es: {}'.format(hipotenusa))
+
